73dce4986921c2bd95ede4eecb8792956f02f2ed
Aaron Sanchez <saarong12@gmail.com>
2024-03-17 16:06:24 -0400

    Removed unnecessary boiler plate and added page and component directories

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardDisplay } from '../pages/card-display/card-display.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CardDisplay],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'pokemon-card-view';
+}
---
+++ src/pages/card-display/card-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'card-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-display.component.html',
+  styleUrl: './card-display.scss',
+})
+export class CardDisplay {
+ title = 'card-display'
+}
