6ebca2da04e38d55f2b9a543ce4c127e079780c0
Aaron Sanchez <saarong12@gmail.com>
2024-03-26 23:19:20 -0500

    Added router functionality.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CardDisplay } from '../pages/card-display/card-display.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: CardDisplay}
+];
