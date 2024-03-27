b9c5075ac92e70e4f6066268a30b1e4880a4afe9
Aaron Sanchez <saarong12@gmail.com>
2024-03-27 00:02:59 -0500

    Fixed the scrollbar display for the card wrapper.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { CardDisplay } from '../pages/card-display/card-display.component';
 
 export const routes: Routes = [
-  {path: "", component: CardDisplay}
+  {path: "", component: CardDisplay},
+  // TODO Add the pokemon details page here
+  // {path:"/details", component: }
 ];
