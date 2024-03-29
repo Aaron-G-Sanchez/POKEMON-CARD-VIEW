b38dfe6b92da7955110049f65c71ea976e3b2a3f
Aaron Sanchez <saarong12@gmail.com>
2024-03-29 13:32:22 -0500

    Restructured app directory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { CardDisplay } from '../pages/card-display/card-display.component';
-import { PokemonDetails } from '../pages/pokemon-details/pokemon-details.component';
+import { CardDisplay } from './pages/card-display/card-display.component';
+import { PokemonDetails } from './pages/pokemon-details/pokemon-details.component';
 
 export const routes: Routes = [
   {path: "", component: CardDisplay},
