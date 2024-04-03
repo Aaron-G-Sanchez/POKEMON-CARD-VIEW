736e4879eba6e0e29e97e948c0061278dcbf82bb
Aaron Sanchez <saarong12@gmail.com>
2024-04-02 21:35:16 -0500

    Added toolbar with menu button

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { MatToolbar } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: "nav-bar",
   standalone: true,
+  imports: [MatToolbar, MatButtonModule, MatIconModule],
   templateUrl: "./nav-bar.component.html",
   styleUrl: "./nav-bar.scss"
 })
