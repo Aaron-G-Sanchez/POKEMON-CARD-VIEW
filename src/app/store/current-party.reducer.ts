acb31ba22669d05e91ee8301794a79bea0ac6f47
Aaron Sanchez <saarong12@gmail.com>
2024-04-05 16:25:55 -0500

    Added am ng template to help differentiate when a party has been created

--- src/app/store/current-party.reducer.ts
+++ src/app/store/current-party.reducer.ts
@@ -1,6 +1,7 @@
 import { createReducer, on } from "@ngrx/store";
 import { ADD_POKEMON } from "./current-party.actions";
 
+// TODO Once pokemon type is written change the any to the new type.
 export const initialState: any[] = []
 
 export const partyReducer = createReducer(
